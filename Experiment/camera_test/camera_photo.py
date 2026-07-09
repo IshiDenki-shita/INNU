12d470b480a0b0df52a87c242c30d5b065852e7a
IshiDenki-shita <e231238@gm.ishikawa-nct.ac.jp>
2026-07-09 11:48:14 +0900

    Revert "wrote simpliest testing code around camera module 2"
    This reverts commit f37fe550155a04531c7b4e60a5806cd2af3cb270.
    
    commited to wrong branch (origin/expe/lidar) by mistake

--- Experiment/camera_test/camera_photo.py
+++ Experiment/camera_test/camera_photo.py
@@ -2,8 +2,10 @@
 ラズパイのカメラモジュールで写真が撮れることを確認
 """
 
+import math
 import time
 from pathlib import Path
+import datetime
 from dataclasses import dataclass
 from typing import List, Tuple
 import numpy as np
